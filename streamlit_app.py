971a0ff0106f5d7ed33bbbdf4690180212f31e75
vasanthapraba <vasanthapraba@gmail.com>
2023-08-01 14:02:33 +0530

    Update streamlit_app.py
    The streamlit app is not working. so I am removing the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,1 @@
 import streamlit
-import pandas
-
-streamlit.title('My Parents New Healthy Dinner')
-
-streamlit.header('🥣Breakfast Menu')
-streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
-
-streamlit.header('🍌🥭 Fruit Smoothie 🥝🍇')
-myfuritlist = pandas.read('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(myfruitlist)
