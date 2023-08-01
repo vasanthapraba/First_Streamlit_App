bd403f768c6a938537a4430a296710a2c4703009
vasanthapraba <vasanthapraba@gmail.com>
2023-08-01 13:57:21 +0530

    Update streamlit_app.py
    added the code back

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit
 
+streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Fruit Smoothie 🥝🍇')
+streamlit.text('🥭Orange Juice')
+streamlit.text('🍇Grape Juice')
