c964409a059f04db2dc610e65e719ecd9d4b82b9
vasanthapraba <vasanthapraba@gmail.com>
2023-08-01 13:52:38 +0530

    Update streamlit_app.py
    Added Furit smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Fruit Smoothie 🥝🍇')
+streamlit.text('🥭Orange Juice')
+streamlit.text('🍇Grape Juice')
