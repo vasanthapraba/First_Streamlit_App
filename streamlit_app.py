4d5d497510f9e819e5492c03497c8c80edd25cf2
vasanthapraba <vasanthapraba@gmail.com>
2023-08-01 14:32:05 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Fruit Smoothie 🥝🍇')
 myfruitlist = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-myfruitlist = my_fruit_list.set_index('Fruit')
+myfruitlist = myfruitlist.set_index('Fruit')
 
 #Allow the user to select some fruits
 streamlit.multiselect("Choose some furits",list(myfruitlist.index))
