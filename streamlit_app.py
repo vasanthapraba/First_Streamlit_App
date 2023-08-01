7d40864942d5c0468c84dc1167334f87831a4732
vasanthapraba <vasanthapraba@gmail.com>
2023-08-01 13:48:38 +0530

    Update streamlit_app.py
    added the tiltle back

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
+streamlit.title('My Parents New Healthy Dinner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
