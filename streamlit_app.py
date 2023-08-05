2019c11b4b5f3cbc78b2f5cf58c8910a6c026db3
vasanthapraba <vasanthapraba@gmail.com>
2023-08-05 22:04:46 +0530

    Update streamlit_app.py
    Updated the code with dataframe to display all the records instead of just one

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ streamlit.dataframe(normalized_json)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load contains")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load contains")
+streamlit.dataframe(my_data_rows)
 
 
